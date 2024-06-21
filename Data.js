68440983aef801bc008185947afc71258c221014
Joey-bergwerff <89838983+derpbearderp@users.noreply.github.com>
2024-06-21 21:19:46 +0200

    Update Data.js

--- Data.js
+++ Data.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   },
   button: {
     position: 'absolute',
-    top: 20, // Adjust bottom position as needed
+    top: 20, 
    
     padding: 10,
     backgroundColor: 'white',
